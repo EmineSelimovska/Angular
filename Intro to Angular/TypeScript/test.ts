b993f713c7c246b9b9fe60ccb8dffbc850777511
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-02-19 23:38:24 +0200

    add exercise for TypeScript

---
+++ Intro to Angular/TypeScript/test.ts
@@ -0,0 +1,61 @@
+let isEven: boolean = false;
+
+type User = {
+    name: string,
+    age: number
+}
+
+const peshoUser = {
+    name: "Pesho",
+    age: 21
+}
+
+interface AnotherUser {
+    firstName: string,
+    id: number,
+
+    getUserName: () => string
+}
+
+// API call
+const anotherUserList = [
+    { firstName: "Ivan", id: 1 },
+    { firstName: "Pavel", id: 2 }
+] as AnotherUser[];
+
+class AnotherUserClass implements AnotherUser {
+    firstName: string;
+    id: number;
+
+    getUserName() {
+        return this.firstName
+    }
+}
+anotherUserList.forEach((firstName, id) => {
+    ////
+
+    console.log(firstName, id);
+
+})
+
+peshoUser.age = 22
+class Human {
+    speak(){
+       console.log("Hello!");
+       
+    }
+}
+
+class Person extends Human {
+    private name: string;
+    public age: number;
+
+    constructor(name: string, age: number){
+        super();
+        this.name = name,
+        this.age = age
+    }
+}
+
+const personPesho = new Person('Pesho', 33);
+personPesho.speak();
