40785606fc175bc611609c39f34594c77be6314e
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-02-20 13:27:01 +0200

    add exercise for TypeScript

---
+++ Intro to Angular/TypeScript/demo.ts
@@ -0,0 +1,22 @@
+// function add(x: number, y: number){
+//     return x + y;
+// }
+
+// let result = add(10,5);
+
+
+let employee: {
+    firstName: "Stoyan",
+    lastName: "Ivanov",
+    age: 23,
+    jobTitle: "Web Developer"
+} =  {
+    firstName: "Stoyan",
+    lastName: "Ivanov",
+    age: 23,
+    jobTitle: "Web Developer"
+}
+
+let employeeObj: {} = {};
+console.log(employeeObj.toString());
+
